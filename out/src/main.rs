3453d8baaa8431c67c4ec37f4986b4067f51821e
Thomas Schollenberger <tschollenberger@sei.cmu.edu>
2023-06-30 15:14:34 -0400

    working number go between, need to go between strings and ints

--- out/src/main.rs
+++ out/src/main.rs
@@ -1,21 +1,28 @@
 use rug::Integer;
 
 fn main() {
-let mut fact: Integer = 111111111111111.into();
-let mut n: Integer = 111.into();
-let mut i: Integer = 111.into();
-let mut ist= String::from("000");
-let mut factst= String::from("000000000000000000");
+    let mut fact: Integer = 111111111111111.into();
+    let mut n: Integer = 111.into();
+    let mut i: Integer = 111.into();
+    let mut ist = String::from("000");
+    let mut factst = String::from("000000000000000000");
 
-n = 16;
-i = 0;
-fact = 1;
-loop {
-if i > n{
-break;}ist = i;
-factst = fact;
-println!("{}{}{}", format!("{}", ist), format!("{}", "! = "), format!("{}", factst));
-i += 1;
-fact *= i;
-}
+    n = 16;
+    i = 0;
+    fact = 1;
+    loop {
+        if i > n {
+            break;
+        }
+        ist = i;
+        factst = fact;
+        println!(
+            "{}{}{}",
+            format!("{}", ist),
+            format!("{}", "! = "),
+            format!("{}", factst)
+        );
+        i += 1;
+        fact *= i;
+    }
 }
